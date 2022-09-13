38592bff2afc45617070721d1ede282692afc93c
sergioogomes <sergiobg2009@hotmail.com>
2022-09-13 12:19:38 -0300

    teste

---
+++ resources/views/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html prefix="og: https://ogp.me/ns#">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>MaisConvite </title>
+
+    <meta property="og:title" content="{{ $event_name }}" />
+    <meta property="og:type" content="image/jpeg" />
+    <meta property="og:description" content="{{ $description }}" />
+    <meta property="og:url" content="{{ $url }}" />
+    <meta property="og:image" content="{{ $image }}" />
+    <meta property="og:image:secure_url" content="{{ $image }}" />
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@400;500;700&display=swap" rel="stylesheet">
+
+  <!-- Bootstrap Icons -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+</head>
+
+<body class="antialiased md:bg-primary-100">
+
+ALGUMA COISA AQUI PARA VER COMO FICA
+
+</body>
+
+</html>
